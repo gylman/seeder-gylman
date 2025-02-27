3595ce570d9af724c27dbacb4d83272234c997f8
Gylman <76925587+gylman@users.noreply.github.com>
2025-02-27 18:14:25 +0900

    Update 01_init_seeder.sh

--- scripts/execute/01_init_seeder.sh
+++ scripts/execute/01_init_seeder.sh
@@ -12,6 +12,12 @@ rm -rf $DATA_PATH/*
 
 $BIN_PATH init --path $DATA_PATH
 
+# Wait until CONFIG_FILE_PATH is available
+while [ ! -f "$CONFIG_FILE_PATH" ]; do
+  echo "Waiting for $CONFIG_FILE_PATH to be created..."
+  sleep 1
+done
+
 sed -i.temp "s|0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80|$SEEDER_PRIVATE_KEY|g" $PRIVATE_KEY_PATH
 
 sed -i.temp "s|seeder_external_rpc_url = \"http://127.0.0.1:6000\"|seeder_external_rpc_url = \"$SEEDER_EXTERNAL_RPC_URL\"|g" $CONFIG_FILE_PATH
