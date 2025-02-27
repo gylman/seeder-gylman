ddc69de7406cc654df9a8f3b1732e1cf8b8f9a47
Gylman <76925587+gylman@users.noreply.github.com>
2025-02-28 08:51:10 +0900

    Update 01_init_seeder.sh

--- scripts/execute/01_init_seeder.sh
+++ scripts/execute/01_init_seeder.sh
@@ -11,7 +11,7 @@ echo "DATA_PATH is set to: $DATA_PATH"
 # rm -rf $DATA_PATH  
 
 # ✅ Instead, remove only the contents
-find $DATA_PATH -mindepth 1 -delete
+# find $DATA_PATH -mindepth 1 -delete
 
 echo "DATA_PATH after rm -rf is $DATA_PATH"
 echo "BIN_PATH after rm -rf is $BIN_PATH"
@@ -29,4 +29,4 @@ sed -i.temp "s|0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff8
 sed -i.temp "s|seeder_external_rpc_url = \"http://127.0.0.1:6000\"|seeder_external_rpc_url = \"$SEEDER_EXTERNAL_RPC_URL\"|g" $CONFIG_FILE_PATH
 sed -i.temp "s|seeder_internal_rpc_url = \"http://127.0.0.1:6001\"|seeder_internal_rpc_url = \"$SEEDER_INTERNAL_RPC_URL\"|g" $CONFIG_FILE_PATH
 
-rm $CONFIG_FILE_PATH.temp
+# rm $CONFIG_FILE_PATH.temp
