48592f82179eb0b3c20ac3a357e659753fe5116f
Gylman <76925587+gylman@users.noreply.github.com>
2025-03-04 17:44:21 +0900

    Update env_example.sh

--- scripts/execute/env_example.sh
+++ scripts/execute/env_example.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CURRENT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PROJECT_ROOT_PATH="$( cd $SCRIPT_PATH/../.. >/dev/null 2>&1 ; pwd -P )"
 
 BIN_FILE_NAME="seeder"
@@ -7,19 +7,22 @@ BIN_PATH="$PROJECT_ROOT_PATH/scripts/$BIN_FILE_NAME"
 
 DATA_PATH=$PROJECT_ROOT_PATH/data
 CONFIG_FILE_PATH=$DATA_PATH/Config.toml
+PRIVATE_KEY_PATH=$DATA_PATH/signing_key
 
 # Copy the new version's binary to the scripts directory
 if [[ -f "$PROJECT_ROOT_PATH/target/release/$BIN_FILE_NAME" ]]; then
-  cp $PROJECT_ROOT_PATH/target/release/$BIN_FILE_NAME $PROJECT_ROOT_PATH/scripts
+          cp $PROJECT_ROOT_PATH/target/release/$BIN_FILE_NAME $PROJECT_ROOT_PATH/scripts
 fi
 
 # Check if the binary exists
 if [[ ! -f "$BIN_PATH" ]]; then
-    echo "Error: Secure RPC binary not found at $BIN_PATH"
-    echo "Please run this command 'cp $PROJECT_ROOT_PATH/target/release/$BIN_FILE_NAME $PROJECT_ROOT_PATH/scripts' after building the project"
-    exit 1
+            echo "Error: Secure RPC binary not found at $BIN_PATH"
+                echo "Please run this command 'cp $PROJECT_ROOT_PATH/target/release/$BIN_FILE_NAME $PROJECT_ROOT_PATH/scripts' after building the project"
+                    exit 1
 fi
 
+# Seeder private key
 SEEDER_PRIVATE_KEY="0x2141478fe814f58de31b5a6fb2a7682b7dae755cc19bab6acdbfa1fcfe6e64e1" # Please change this.
+
 SEEDER_EXTERNAL_RPC_URL="http://127.0.0.1:6000" # External IP - Please change this IP.
-SEEDER_INTERNAL_RPC_URL="http://127.0.0.1:6001" # Internal IP - Please change this IP.
+SEEDER_INTERNAL_RPC_URL="http://127.0.0.1:6001"  # Internal IP - Please change this IP.
